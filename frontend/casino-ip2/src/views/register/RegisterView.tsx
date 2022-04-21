952249db4ebf2830b71fcf060dbb435fb3cbb947
MoritzZucker <62714043+MoritzZucker@users.noreply.github.com>
2022-04-21 14:14:05 +0200

    reg1 test

--- frontend/casino-ip2/src/views/register/RegisterView.tsx
+++ frontend/casino-ip2/src/views/register/RegisterView.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Register from "../../components/forms/register"
+import './style/Register.css'
 
 function RegisterView() {
 	return (
-	
+		<div>
+
+   		<div id="container"/>
+		<script>var mountNode = document.getElementById('container');</script>
+
 		<div className="content">
 			<Register />
-		</div>		
+		</div>	
+		</div>	
 	);
 }
+
+
 
 export default RegisterView;
