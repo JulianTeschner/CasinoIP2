b3d202c0c1979fb0bc3580ff7d0c2a1678c23bd9
Marco Haas <92hama1bif@hft-stuttgart.de>
2022-04-27 15:07:58 +0200

    [TASK-33] implement game-overview for blackjack and slotmachine

--- frontend/casino-ip2/src/views/overview/Overview.tsx
+++ frontend/casino-ip2/src/views/overview/Overview.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Row, Col, Card, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import { RouteName } from "../../routes/routesnames";
+
+const { Meta } = Card;
+const { Title } = Typography;
 
 function Overview() {
 	return (
-		<div>
-            Overview
-        </div>
+		<Row>
+      		<Col span={12} offset={6}>
+				<div className="Overview">
+					<Title>
+            			Overview
+        			</Title>
 
+					<Title level={2}>
+						Games
+					</Title>
+					<Row gutter={[18,18]}>
+						<Col sm={{span:24}} md={{span:12}}>
+							<Link to={`${RouteName.HOME}`}>
+								<Card hoverable
+									cover={<img alt="blackjack" src={require('./images/blackjack.jpg')} height="260px"/>}>
+										<Meta title="Blackjack" />
+								</Card>
+          					</Link>
+						</Col>
+						<Col sm={{span:24}} md={{span:12}}>
+							<Link to={`${RouteName.HOME}`}>
+								<Card hoverable 
+									cover={<img alt="slotmachine" src={require('./images/slotmaschine.jpg')} height="260px"/>}>
+										<Meta title="Slotmachine" />
+								</Card>
+          					</Link>
+						</Col>
+					</Row>
+				</div>
+      		</Col>
+    	</Row>
 		
+		
+
 	);
 }
 
--- frontend/casino-ip2/src/views/overview/__tests__/Overview.spec.tsx
+++ frontend/casino-ip2/src/views/overview/__tests__/Overview.spec.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { render, screen } from "@testing-library/react";
 import Overview from "../Overview";
+import { AllProviders } from '../../../testUtils';
 
 describe("Overview", () => {
-	it("renders a div", async () => {
-		render(<Overview/>);
+	it("should render the Overview", async () => {
+		render(
+			<AllProviders>
+				<Overview/>
+			</AllProviders>
+		);
 
-		const findDiv = await screen.findByText(/Overview/i);
+		const findTitle = await screen.findByText(/Overview/i);
+		const findSubTitle = await screen.findByText(/Games/i);
 
-		expect(findDiv).toBeInTheDocument();
+		const findBlackjack = await screen.findByText(/Blackjack/i);
+		const findSlotmachine = await screen.findByText(/Slotmachine/i);
+
+		const findBlackjackImage = screen.getAllByAltText('blackjack');
+		const findSlotmachineImage = screen.getAllByAltText('slotmachine');
+
+		expect(findTitle).toBeInTheDocument();
+		expect(findSubTitle).toBeInTheDocument();
+		expect(findBlackjack).toBeInTheDocument();
+		expect(findSlotmachine).toBeInTheDocument();
+		//expect(findBlackjackImage).toHaveAttribute('src', './images/blackjack.jpg');
+		//expect(findSlotmachineImage).toHaveAttribute('src','./images/slotmachine.jpg');
 	});
 });
