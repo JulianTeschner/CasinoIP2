3cc9ccccc05523fb830da8ea6daec6ceb41845cd
MoritzZucker <62714043+MoritzZucker@users.noreply.github.com>
2022-04-28 14:12:27 +0200

    Basic

---
+++ frontend/casino-ip2/src/views/slotmachine/Slotmachine.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import SlotmachineView from './SlotmachineView';
+
+
+function Slotmachine() {
+    return (
+     <div className="Slotmachine">
+       
+     </div>
+    );
+  }
+
+
+export default Slotmachine;
