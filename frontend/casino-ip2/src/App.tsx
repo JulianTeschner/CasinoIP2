69a95cb988b71de5fb31924d7b45cea0a9657ed6
kilian <kilian.hammer@gmx.de>
2022-05-05 21:13:26 +0200

    translated to english, added icon to cards

--- frontend/casino-ip2/src/App.tsx
+++ frontend/casino-ip2/src/App.tsx
@@ -2,12 +2,13 @@ import RootRoutes from "./routes/root.routes";
 import './App.less';
 import { ThemeProvider } from "@emotion/react";
 import defaultTheme from "./themes/default";
+import Blackjack from "./views/blackjack/Blackjack";
 
 function App() {
   return (
    <div className="App">
      <ThemeProvider theme={defaultTheme}>
-       <RootRoutes />
+       <Blackjack />
      </ThemeProvider>
    </div>
   );
