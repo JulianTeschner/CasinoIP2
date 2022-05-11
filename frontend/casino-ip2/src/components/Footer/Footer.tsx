9d3b7e4bd21bad345c9973af243278c28ddc7eec
Marco Haas <92hama1bif@hft-stuttgart.de>
2022-05-11 18:09:50 +0200

    [TASK-43] initial darkmode implementation

--- frontend/casino-ip2/src/components/Footer/Footer.tsx
+++ frontend/casino-ip2/src/components/Footer/Footer.tsx
@@ -1,15 +1,29 @@
-import { Row } from 'antd';
+import { Row, Switch } from 'antd';
+import React from 'react';
+import { useThemeSwitcher } from 'react-css-theme-switcher';
 import { Link } from 'react-router-dom';
 import { RouteName } from '../../routes/routesnames';
 
 export default function Footer() {
-    const currentDate = new Date();
+    const [isDarkMode, setIsDarkMode] = React.useState();
+    const { switcher, currentTheme, status, themes } = useThemeSwitcher();
+
+    const toggleTheme = (isChecked:any) => {
+        setIsDarkMode(isChecked);
+        switcher({ theme: isChecked ? themes.dark: themes.light });
+    };
+
+    if (status === "loading"){
+        return null;
+    }
 
     return (
         <Row justify="center">
             <Link to={`${RouteName.ABOUTUS}`}>
                 <small>Internetprogrammierung Sommersemester 2022</small>
             </Link>
+            <Switch style={{position: 'absolute', right: '20px'}} checked={isDarkMode} onChange={toggleTheme} />
         </Row>
+
     )
 }
