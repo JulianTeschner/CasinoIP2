4a49993f07219b1f76e755b0921e3e5c166d298a
Marco Haas <92hama1bif@hft-stuttgart.de>
2022-06-15 14:26:39 +0200

    [Task-depositMoney] update tests and implement backend call

--- frontend/casino-ip2/src/views/balance/__tests__/Balance.spec.tsx
+++ frontend/casino-ip2/src/views/balance/__tests__/Balance.spec.tsx
@@ -19,6 +19,17 @@ describe('logout view', () => {
       btn.click();
 
       const logoutText = await screen.findByTestId('balance-text');
+      const btnDeposit = await screen.findByTestId('balance-button');
+      const findFirstname = await screen.findByText('First Name');
+		  const findLastname = await screen.findByText('Last Name');
+      const findCredit = await screen.findByText('Creditcard-Number');
+		  const findDeposit = await screen.findByTestId('deposit-deposit');
+
       expect(logoutText).toBeInTheDocument();
+      expect(btnDeposit).toBeInTheDocument();
+      expect(findFirstname).toBeInTheDocument();
+      expect(findLastname).toBeInTheDocument();
+      expect(findCredit).toBeInTheDocument();
+      expect(findDeposit).toBeInTheDocument();
     });
 })
