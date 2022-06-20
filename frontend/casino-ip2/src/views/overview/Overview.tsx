8844b6c49be62c91fd87e1fa125315a7c58f323c
Marco Haas <92hama1bif@hft-stuttgart.de>
2022-06-20 14:15:15 +0200

    [Bug] responsive images in overview

--- frontend/casino-ip2/src/views/overview/Overview.tsx
+++ frontend/casino-ip2/src/views/overview/Overview.tsx
@@ -22,7 +22,7 @@ function Overview() {
 						<Col sm={{span:24}} md={{span:12}}>
 							<Link to={`${RouteName.BLACKJACK}`} data-testid="blackjack-link">
 								<Card hoverable
-									cover={<img alt="blackjack" src={require('./images/blackjack.jpg')} height="260px" data-testid="blackjack-card"/>}>
+									cover={<img alt="blackjack" src={require('./images/blackjack.jpg')} data-testid="blackjack-card"/>}>
 										<Row justify="center">
 											<Meta title="Blackjack" />
 										</Row>
@@ -32,7 +32,7 @@ function Overview() {
 						<Col sm={{span:24}} md={{span:12}}>
 							<Link to={`${RouteName.SLOTMACHINE}`} data-testid="slotmachine-link">
 								<Card hoverable 
-									cover={<img alt="slotmachine" src={require('./images/slotmaschine.jpg')} height="260px" data-testid="slotmachine-card"/>}>
+									cover={<img alt="slotmachine" src={require('./images/slotmaschine.jpg')} data-testid="slotmachine-card"/>}>
 										<Row justify="center">
 											<Meta title="Slotmachine" />
 										</Row>
@@ -43,7 +43,7 @@ function Overview() {
 						<Col sm={{span:24}} md={{span:12}}>
 							<Link to={`${RouteName.SPORTBET}`} data-testid="sportbet-link">
 								<Card hoverable 
-									cover={<img alt="sportbet" src={require('./images/sportbet.jpg')} height="260px" data-testid="sportbet-card"/>}>
+									cover={<img alt="sportbet" src={require('./images/sportbet.jpg')} data-testid="sportbet-card"/>}>
 										<Row justify="center">
 											<Meta title="Sportbet" />
 										</Row>
